69c3f5e3447835588f030718037dde3a5dd3f99a
John Rigby <john.rigby@linaro.org>
2011-08-16 00:58:42 -0600

    OMAP3 overo: fix gpio.h build errors
    Signed-off-by: John Rigby <john.rigby@linaro.org>

--- board/overo/overo.c
+++ board/overo/overo.c
@@ -36,7 +36,7 @@
 #include <asm/arch/mux.h>
 #include <asm/arch/mem.h>
 #include <asm/arch/sys_proto.h>
-#include <asm/arch/gpio.h>
+#include <asm/omap_gpio.h>
 #include <asm/mach-types.h>
 #include "overo.h"
 
